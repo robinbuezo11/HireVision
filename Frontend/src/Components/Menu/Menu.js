4d9333defae70e1379d4c48e392c3faee45f67bc
Brandon Tejaxun <brandonteja02@gmail.com>
2024-10-12 01:01:58 -0600

    [update] menu

--- Frontend/src/Components/Menu/Menu.js
+++ Frontend/src/Components/Menu/Menu.js
@@ -5,6 +5,9 @@ import './Menu.css';
 const Menu = ({ isOpen }) => {
     return (
         <div className={`menu ${isOpen ? 'show' : ''}`}>
+            <div className='profile'>
+                <img src='/Profile.svg' alt='User Icon' />
+            </div>
             <ul className='menu-options'>
                 <li className='menu-item'>
                     <FaBriefcase className='menu-icon' /> Empleos
