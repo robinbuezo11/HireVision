f6b68f082466a5a0baa59545ee8119d41ef15574
Brandon Tejaxun <brandonteja02@gmail.com>
2024-10-15 16:57:38 -0600

    [update] gráfica de porcentaje

--- Frontend/src/Components/Panels/JobApplications.js
+++ Frontend/src/Components/Panels/JobApplications.js
@@ -8,28 +8,32 @@ const DonutChart = ({ percentage }) => {
         { name: 'Remaining', value: 100 - percentage }
     ];
 
-    const COLORS = ['#5243F5', '#F5F5F5']; // Personaliza los colores
+    const COLORS = ['#5243F5', '#F5F5F5'];
 
     return (
-        <PieChart width={100} height={100}>
-        <Pie
-            data={data}
-            cx={50}
-            cy={50}
-            innerRadius={30}
-            outerRadius={40}
-            startAngle={90}
-            endAngle={450}
-            paddingAngle={5}
-            dataKey="value"
-        >
-            {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index]} />
-            ))}
-        </Pie>
-        </PieChart>
+        <div className="graph-container">
+            <PieChart width={120} height={120}>
+                <Pie
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={40}
+                    outerRadius={50}
+                    startAngle={90}
+                    endAngle={450}
+                    paddingAngle={5}
+                    dataKey="value"
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index]} />
+                    ))}
+                </Pie>
+            </PieChart>
+            <div className="percentage">{percentage}%</div>
+        </div>
     );
 };
+
 
 const JobApplications = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
