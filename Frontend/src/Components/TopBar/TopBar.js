71367c2f09332eb5f99e30910818878197d783f0
KevinPalaciosQ <3001877180101@ingenieria.usac.edu.gt>
2024-10-15 20:12:35 -0600

    edit profile done

--- Frontend/src/Components/TopBar/TopBar.js
+++ Frontend/src/Components/TopBar/TopBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { FaSearch, FaUser } from 'react-icons/fa';
 import { IoMenu, IoClose } from 'react-icons/io5';
 import { IoIosLogOut } from "react-icons/io";
@@ -7,14 +7,18 @@ import logo from './UD.png';
 
 const TopBar = ({ toggleMenu, menuOpen }) => {
     const [dropdownVisible, setDropdownVisible] = useState(false);
-    const [isProfileOpen, setIsProfileOpen] = useState(false); // Estado para controlar la ventana emergente
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
+    const [isEditProfile, setIsEditProfile] = useState(false);
+    const [profilePicture, setProfilePicture] = useState('/Profile.svg');
+    
+    const fileInputRef = useRef(null);
 
     const toggleDropdown = () => {
         setDropdownVisible(!dropdownVisible);
     };
 
     const handleViewProfile = () => {
-        setIsProfileOpen(true); // Mostrar la ventana emergente
+        setIsProfileOpen(true);
     };
 
     const handleLogout = () => {
@@ -23,7 +27,32 @@ const TopBar = ({ toggleMenu, menuOpen }) => {
     };
 
     const closeProfileModal = () => {
-        setIsProfileOpen(false); // Cerrar la ventana emergente
+        setIsProfileOpen(false);
+        setIsEditProfile(false);
+    };
+
+    const handleEditClick = () => {
+        setIsEditProfile(true);
+    };
+
+    const handleProfilePictureChange = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setProfilePicture(reader.result);  // Actualiza la imagen
+        };
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handlePictureClick = () => {
+        fileInputRef.current.click(); // Al hacer clic en la imagen, abre el input file
+    };
+
+    const handleSaveChanges = () => {
+        console.log('Cambios guardados');
+        closeProfileModal();
     };
 
     return (
@@ -45,7 +74,7 @@ const TopBar = ({ toggleMenu, menuOpen }) => {
             </div>
             <div className={`user-profile ${dropdownVisible ? 'active' : ''}`} onClick={toggleDropdown}>
                 <div className='user-icon'>
-                    <img src='/Profile.svg' alt='User Profile'/>
+                    <img src={profilePicture} alt='User Profile' className='profile-picture-circle'/>
                 </div>
                 <span className='user-name'>Administrador</span>
 
@@ -59,26 +88,86 @@ const TopBar = ({ toggleMenu, menuOpen }) => {
                 }
             </div>
 
-            {/* Ventana emergente para ver el perfil */}
             {isProfileOpen && (
                 <div className='edit-profile-modal'>
                     <div className='edit-profile-content'>
-                        <h2>Mi Perfil</h2>
-                        {/* Botón "X" para cerrar */}
+                        <h2>{isEditProfile ? 'Editar Perfil' : 'Mi Perfil'}</h2>
                         <button className='close-btn' onClick={closeProfileModal}>X</button>
-                        <div className='profile-picture'>
-                            <img src='/Profile.svg' alt='Imagen de perfil' />
-                        </div>
-                        <div>
-                            <p>Administrador</p> {/* Usuario quemado */}
-                        </div>
-                        <div>
-                            <p>admin@hirevision.com</p> {/* Correo quemado */}
-                        </div>
-                        <div>
-                            <p>01/01/1990</p> {/* fecha quemada */}
-                        </div>
-                        <button className='save-btn'>Editar</button>
+
+                        {!isEditProfile ? (
+                            <>
+                                {/* Sección de visualización del perfil */}
+                                <div className='profile-picture'>
+                                    <img
+                                        src={profilePicture}
+                                        alt='Imagen de perfil'
+                                        onClick={handlePictureClick}
+                                        className='profile-picture-circle'
+                                    />
+                                    <input
+                                        type='file'
+                                        ref={fileInputRef}
+                                        style={{ display: 'none' }}
+                                        onChange={handleProfilePictureChange}
+                                    />
+                                </div>
+                                <div>
+                                    <p>Administrador</p>
+                                </div>
+                                <div>
+                                    <p>admin@hirevision.com</p>
+                                </div>
+                                <div>
+                                    <p>01/01/1990</p>
+                                </div>
+                                <button className='save-btn' onClick={handleEditClick}>Editar</button>
+                            </>
+                        ) : (
+                            <>
+                                {/* Sección de edición del perfil */}
+                                <div className='profile-picture'>
+                                    <img
+                                        src={profilePicture} // Mostrar la imagen actualizada
+                                        alt='Imagen de perfil'
+                                        onClick={handlePictureClick}
+                                        className='profile-picture-circle'
+                                    />
+                                    <input
+                                        type='file'
+                                        ref={fileInputRef}
+                                        style={{ display: 'none' }}
+                                        onChange={handleProfilePictureChange}
+                                    />
+                                </div>
+                                <form className='columns'>
+                                    <div className="form-group">
+                                        <input type='text' id='name' name='name' required placeholder=" " />
+                                        <label htmlFor='name'>Nombre</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type='text' id='last-name' name='last-name' required placeholder=" " />
+                                        <label htmlFor='last-name'>Apellido</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type='email' id='email' name='email' required placeholder=" " />
+                                        <label htmlFor='email'>Correo Electrónico</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type='date' id='birthdate' name='birthdate' required placeholder=" " />
+                                        <label htmlFor='birthdate'>Fecha de Nacimiento</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type='password' id='password' name='password' required placeholder=" " />
+                                        <label htmlFor='password'>Nueva Contraseña</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type='password' id='confirm-password' name='confirm-password' required placeholder=" " />
+                                        <label htmlFor='confirm-password'>Confirmar Contraseña</label>
+                                    </div>
+                                    <button type='button' className='full-width' onClick={handleSaveChanges}>Guardar Cambios</button>
+                                </form>
+                            </>
+                        )}
                     </div>
                 </div>
             )}
